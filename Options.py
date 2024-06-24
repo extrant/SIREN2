d22d8eec1b2558d5eddeecc25cb9f86fac773d00
NicoCHANY <extrant@yeah.net>
2024-06-24 17:29:24 +0800

    Forever

--- Options.py
+++ Options.py
@@ -1,8 +1,8 @@
 use_network = True  #如果导致卫月报错，且你必须要使用卫月。请将此项改成Falae，反之则为True
 #请注意修改后有关网络包的功能都会被禁用！
 
-auto_update = True  #自动更新开关，讨厌烦人的更新检测？改成False就可以了，但是你要注意的是修改后就不能获取到最新的版本了。
+auto_update = False  #自动更新开关，讨厌烦人的更新检测？改成False就可以了，但是你要注意的是修改后就不能获取到最新的版本了。
 
-auto_update_method = 0
+auto_update_method = 1
 # 0为默认
 # 1为通过Github代理以及清华源进行更新
